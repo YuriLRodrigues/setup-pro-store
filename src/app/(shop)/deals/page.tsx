ffd3fbc10baa845a3266fd02296bb9fad740185e
YuriLRodrigues <rodriguesyuri769@gmail.com>
2025-02-26 15:43:11 -0300

    feat: :sparkles: page of product by category, deals and orders pages with your components separatelly

---
+++ src/app/(shop)/deals/page.tsx
@@ -0,0 +1,37 @@
+import { Badge } from "@/components/ui/badge";
+import ProductItem from "@/components/ui/product-item";
+import { computeProductTotalPrice } from "@/helpers/product";
+import { prismaClient } from "@/lib/prisma";
+import { PercentIcon } from "lucide-react";
+
+const DealsPage = async () => {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+
+  return (
+    <div className="container mx-auto p-5">
+      <Badge variant="heading">
+        <PercentIcon size={16} />
+        Ofertas
+      </Badge>
+      <div className="grid grid-cols-2 gap-8 pt-5 md:grid-cols-3 lg:grid-cols-4">
+        {deals.map((product) => (
+          <ProductItem
+            key={product.id}
+            product={{
+              ...product,
+              totalPrice: computeProductTotalPrice(product),
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DealsPage;
---
+++ src/app/(shop)/product/[slug]/components/product-info.tsx
@@ -0,0 +1,151 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import DiscountBadge from "@/components/ui/discount-badge";
+import { useToast } from "@/components/ui/use-toast";
+import { ProductWithTotalPrice } from "@/helpers/product";
+import { CartContext } from "@/providers/cart";
+import { HeartIcon } from "lucide-react";
+import { ChevronLeftIcon, ChevronRightIcon, TruckIcon } from "lucide-react";
+import { useContext, useState, useEffect } from "react";
+
+interface ProductInfoProps {
+  product: ProductWithTotalPrice;
+}
+const ProductInfo = ({ product }: ProductInfoProps) => {
+  const [quantity, setQuantity] = useState(1);
+  const [isInWishlist, setIsInWishlist] = useState(false);
+
+  const { toast } = useToast();
+
+  const {
+    addProductToCart,
+    addProductToWishList,
+    removeProductFromWishlist,
+    wishlist,
+  } = useContext(CartContext);
+
+  useEffect(() => {
+    // Verificar se o produto já está na wishlist
+    setIsInWishlist(
+      wishlist.some((wishlistItem) => wishlistItem.id === product.id),
+    );
+  }, [wishlist, product.id]);
+
+  const handleDecreaseQuantityClick = () => {
+    setQuantity((prev) => (prev === 1 ? prev : prev - 1));
+  };
+
+  const handleIncreaseQuantityClick = () => {
+    setQuantity((prev) => prev + 1);
+  };
+
+  const handleAddToWishList = () => {
+    if (isInWishlist) {
+      // Se o produto já está na wishlist, remova-o
+      removeProductFromWishlist(product.id);
+      setIsInWishlist(false);
+      toast({
+        title: "Sucesso!",
+        description: "Item removido dos Favoritos",
+      });
+    } else {
+      // Se o produto não está na wishlist, adicione-o
+      addProductToWishList({ ...product, quantity });
+      setIsInWishlist(true);
+      toast({
+        title: "Sucesso!",
+        description: "Item adicionado aos Favoritos",
+      });
+    }
+  };
+
+  const handleAddToCartClick = () => {
+    addProductToCart({ ...product, quantity });
+    toast({
+      title: "Sucesso!",
+      description: "Item adicionado ao carrinho",
+    });
+  };
+
+  return (
+    <div className="flex flex-col px-5 lg:w-[40%] lg:rounded-lg lg:bg-accent lg:p-10">
+      <div className="flex justify-between">
+        <h2 className="text-lg lg:text-2xl">{product.name}</h2>
+        <div className=" bottom-3 right-3 cursor-pointer">
+          <HeartIcon
+            onClick={handleAddToWishList}
+            fill={isInWishlist ? "#BA0001" : "none"}
+            color={isInWishlist ? "#BA0001" : "white"}
+            size={24}
+          />
+        </div>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <h1 className="text-xl font-bold">
+          R$ {product.totalPrice.toFixed(2)}
+        </h1>
+        {product.discountPercentage > 0 && (
+          <DiscountBadge>{product.discountPercentage}</DiscountBadge>
+        )}
+      </div>
+      {product.discountPercentage > 0 && (
+        <p className="text-sm line-through opacity-75">
+          De: R$ {Number(product.basePrice).toFixed(2)}
+        </p>
+      )}
+
+      <div className="mt-4 flex items-center gap-2">
+        <Button
+          onClick={handleDecreaseQuantityClick}
+          size="icon"
+          variant="outline"
+        >
+          <ChevronLeftIcon size={16} />
+        </Button>
+
+        <span>{quantity}</span>
+
+        <Button
+          onClick={handleIncreaseQuantityClick}
+          size="icon"
+          variant="outline"
+        >
+          <ChevronRightIcon size={16} />
+        </Button>
+      </div>
+
+      <div className="mt-8 flex flex-col gap-3">
+        <h3 className="font-bold">Descrição</h3>
+        <p className="text-justify text-sm opacity-60">{product.description}</p>
+      </div>
+
+      <Button
+        className="mt-8 font-bold uppercase"
+        onClick={handleAddToCartClick}
+      >
+        Adicionar ao carrinho
+      </Button>
+
+      <div className="mt-6 flex items-center justify-between rounded-lg bg-accent px-5 py-2 lg:bg-[#2A2A2A]">
+        <div className="flex items-center gap-2">
+          <TruckIcon />
+
+          <div className="flex flex-col">
+            <p className="text-xs">
+              Entrega via <span className="font-bold">FSPacket</span>
+            </p>
+            <p className="text-xs text-[#8162ff]">
+              Envio para <span className="font-bold">todo Brasil</span>
+            </p>
+          </div>
+        </div>
+
+        <p className="text-xs font-bold">Frete Grátis</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductInfo;
